856607a95e81c3de14a7e782e67dbbc5979d405d
Theanchhing <45730153+Theanchhing@users.noreply.github.com>
2022-09-20 04:12:23 +0700

    add experience content

--- components/Main.jsx
+++ components/Main.jsx
@@ -23,9 +23,7 @@ const Main = () => {
             Hi, I&#39;m <span className='text-[#098DD7]'> THEANCHHING</span>
           </h1>
           <h1 className='py-2 text-gray-700'>
-          {/* <span>{text}</span> */}
-          {/* <Cursor /> */}
-          
+            A Frontend Web & Mobile Developer
           </h1>
           <p className='py-4 text-gray-600 sm:max-w-[70%] m-auto'>
             I’m focused on building responsive front-end web applications while
